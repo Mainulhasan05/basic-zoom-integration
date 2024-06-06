5c67742bede6cb7e154a6c2d26fbc92150db4b63
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-06-07 03:43:18 +0600

    add

---
+++ meetingsdk-react-sample/src/App.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import './App.css';
+import { ZoomMtg } from '@zoom/meetingsdk';
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+
+function App() {
+
+  var authEndpoint = 'http://localhost:4000'
+  var sdkKey = 'WjVrtAtISuya6Js7kF0YfQ'
+  var meetingNumber = '74900294440'
+  var passWord = 'Sy2LdD'
+  var role = 0
+  var userName = 'AI Arif'
+  var userEmail = 'mdrifatbd5@gmail.com'
+  var registrantToken = ''
+  var zakToken = ''
+  var leaveUrl = 'http://localhost:3000'
+
+  function getSignature(e) {
+    e.preventDefault();
+
+    fetch(authEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        meetingNumber: meetingNumber,
+        role: role
+      })
+    }).then(res => res.json())
+    .then(response => {
+      startMeeting(response.signature)
+    }).catch(error => {
+      console.error(error)
+    })
+  }
+
+  function startMeeting(signature) {
+    document.getElementById('zmmtg-root').style.display = 'block'
+
+    ZoomMtg.init({
+      leaveUrl: leaveUrl,
+      patchJsMedia: true,
+      leaveOnPageUnload: true,
+      success: (success) => {
+        console.log(success)
+
+        ZoomMtg.join({
+          signature: signature,
+          sdkKey: sdkKey,
+          meetingNumber: meetingNumber,
+          passWord: passWord,
+          userName: userName,
+          userEmail: userEmail,
+          tk: registrantToken,
+          zak: zakToken,
+          success: (success) => {
+            console.log(success)
+          },
+          error: (error) => {
+            console.log(error)
+          }
+        })
+
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+
+  return (
+    <div className="App">
+      <main>
+        <h1>Zoom Meeting SDK Sample React</h1>
+
+        <button onClick={getSignature}>Join Meeting</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
